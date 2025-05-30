fbeb2db04ddf77c1c80522e353e2c3d9ef910042
AuricViolet <matthew.k.1156@gmail.com>
2025-05-30 16:06:19 -0300

    Add files via upload

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,11 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace TestApp;
 
@@ -7,5 +14,36 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    public void ClickHandler(object sender, RoutedEventArgs args)
+    {
+        if (sender is Button clickedButton)
+        {
+            switch (clickedButton.Name)
+            {
+                case "Login":
+                    if ((Username.Text == "Migratedwolf") && (Password.Text == "Echo123e#"))
+                    {
+                        clickedButton.Content = "Logout";
+                    }
+                    break;
+                case "Signup":
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    {
+                        // Use cmd to launch the default browser
+                        Process.Start(new ProcessStartInfo("cmd", $"/c start https://www.fredericton.ca/en")
+                        {
+                            CreateNoWindow = true
+                        });
+                    }
+                    else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    {
+                        // Use xdg-open to launch default browser
+                        Process.Start("xdg-open", "https://www.fredericton.ca/en");
+                    }
+                    break;
+            }
+
+        }
     }
 }
